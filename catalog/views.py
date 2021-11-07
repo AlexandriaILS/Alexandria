e9627e316881099173db49d48c44b00c44976073
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-11-06 21:41:57 -0400

    fix bug with searching from homepage

--- catalog/views.py
+++ catalog/views.py
@@ -72,7 +72,7 @@ def search(request: WSGIRequest) -> HttpResponse:
                 )
             )
             .exclude(id__in=Record.objects.filter(item__isnull=True))
-            .order_by(Lower("title"))
+            .order_by(Lower("title")).distinct()
         )
     results_per_page = get_results_per_page(request)
 
