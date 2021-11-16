a6a4017f15b89296b82d5ac2ff82182f8c7b3874
Joe Kaufeld <joe.kaufeld@gmail.com>
2021-11-15 21:02:31 -0500

    add address for default user

--- catalog/management/commands/bootstrap_site.py
+++ catalog/management/commands/bootstrap_site.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from catalog.management.commands import bootstrap_types, bootstrap_system_branches
 
+from users.models import USLocation
+
 
 class Command(BaseCommand):
     help = "Creates everything needed for the site to be functional."
@@ -9,12 +11,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         bootstrap_types.Command().handle()
         bootstrap_system_branches.Command().handle()
+
+        location = USLocation.objects.get_or_create(
+            address_1="123 Sesame St.",
+            city="Kaufman Astoria Studios",
+            state="NY",
+            zip_code="11106",
+        )
         user, created = get_user_model().objects.get_or_create(
             card_number="1234",
             is_staff=True,
             is_superuser=True,
             first_name="Admin",
             last_name="von Admin",
+            address=location
         )
         if created:
             user.set_password("asdf")
