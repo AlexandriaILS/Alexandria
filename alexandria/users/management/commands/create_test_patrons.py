9e0fd712dc02d9367a490429c69f77068fcf39f8
Joe Kaufeld <opensource@joekaufeld.com>
2022-03-02 23:42:35 -0500

    completely revamp create_test_patrons

--- alexandria/users/management/commands/create_test_patrons.py
+++ alexandria/users/management/commands/create_test_patrons.py
@@ -1,11 +1,10 @@
 import random
 import string
 from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+
 from alexandria.users.models import User, USLocation, BranchLocation
 
 try:
@@ -59,11 +58,43 @@ class Command(BaseCommand):
             f" some minutes. Please plan accordingly."
         )
 
-        with ThreadPoolExecutor() as executor:
-            jobs = list()
-            for _ in range(count):
-                jobs.append(executor.submit(create_user, _))
-            for f in as_completed(jobs):
-                pass
+        person = Person()
+        address = Address()
+
+        addresses = []
+        people = []
+
+        valid_branches = BranchLocation.objects.filter(
+            open_to_public=True, host=settings.DEFAULT_HOST_KEY
+        )
+
+        self.stdout.write("Generating locations...")
+
+        for x in range(count):
+            addresses.append(USLocation(
+                address_1=address.address(),
+                city=address.city(),
+                state=address.state(),
+                zip_code=address.zip_code(),
+            ))
+        USLocation.objects.bulk_create(addresses)
+
+        self.stdout.write("Generating patrons...")
+        for x in range(count):
+            age = person.age(minimum=0)
+
+            people.append(User(
+                card_number="".join([random.choice(string.digits) for _ in range(14)]),
+                address=USLocation.objects.get(id=x+2),  # account for zero and the admin address
+                first_name=person.first_name(),
+                last_name=person.last_name(),
+                email=person.email(),
+                birth_year=datetime.now().year - age,
+                is_minor=True if age < 18 else False,
+                is_staff=False,
+                default_branch=random.choice(valid_branches),
+            ))
+
+        User.objects.bulk_create(people)
 
         self.stdout.write(self.style.SUCCESS(f"Created {str(count)} new patrons!"))
