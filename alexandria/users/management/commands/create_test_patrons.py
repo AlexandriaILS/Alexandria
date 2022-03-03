b828e5dc336913f406b291e4492c8dacde3fa9e4
Joe Kaufeld <opensource@joekaufeld.com>
2022-03-02 23:43:04 -0500

    remove dead code

--- alexandria/users/management/commands/create_test_patrons.py
+++ alexandria/users/management/commands/create_test_patrons.py
@@ -14,35 +14,6 @@ except ImportError:
     raise CommandError("Cannot proceed; missing dev dependencies.")
 
 
-def create_user(*args):
-    person = Person()
-    address = Address()
-
-    location = USLocation.objects.create(
-        address_1=address.address(),
-        city=address.city(),
-        state=address.state(),
-        zip_code=address.zip_code(),
-    )
-    age = person.age(minimum=0)
-    valid_branches = BranchLocation.objects.filter(
-        open_to_public=True, host=settings.DEFAULT_HOST_KEY
-    )
-    newbie = User.objects.create(
-        card_number="".join([random.choice(string.digits) for _ in range(14)]),
-        address=location,
-        first_name=person.first_name(),
-        last_name=person.last_name(),
-        email=person.email(),
-        birth_year=datetime.now().year - age,
-        is_minor=True if age < 18 else False,
-        is_staff=False,
-        default_branch=random.choice(valid_branches),
-    )
-    newbie.set_password("asdf")
-    newbie.save()
-
-
 class Command(BaseCommand):
     help = "Creates X (int) number of patrons for tests."
 
