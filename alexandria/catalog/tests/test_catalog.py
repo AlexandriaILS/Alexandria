76139623f111110a9d78bef4b9b0e154e07de736
Joe Kaufeld <opensource@joekaufeld.com>
2022-04-12 19:47:54 -0400

    minor cleanup

---
+++ alexandria/catalog/tests/test_catalog.py
@@ -0,0 +1,61 @@
+from django.test import Client
+from django.urls import reverse
+
+from alexandria.utils.test_helpers import get_default_record, get_test_item
+
+
+def test_index(client: Client):
+    response = client.get(reverse("homepage"))
+    assert response.status_code == 200
+
+
+def test_index_post(client: Client):
+    response = client.post(reverse("homepage"), data={"search_text": "wazzzup"})
+    assert response.status_code == 302
+    assert response.url == "/search/?q=wazzzup"
+
+
+def test_index_post_without_search_query(client: Client):
+    response = client.post(reverse("homepage"), data={"search_text": ""})
+    assert response.status_code == 302
+    assert response.url == "/search/?q="
+
+
+class TestCatalog:
+    def test_empty_catalog(self, client: Client):
+        response = client.get(reverse("search"))
+        assert response.status_code == 200
+
+    def test_catalog_search(self, client: Client):
+        test_record = get_default_record()
+        get_test_item(record=test_record)
+
+        response = client.get(reverse("search"), data={"q": test_record.title})
+        assert response.status_code == 200
+        paginator_results = response.context["page"].paginator
+        assert len(paginator_results.object_list) == 1
+        assert test_record in paginator_results.object_list
+
+    def test_catalog_search_without_item(self, client: Client):
+        test_record = get_default_record()
+
+        response = client.get(reverse("search"), data={"q": test_record.title})
+        assert response.status_code == 200
+        paginator_results = response.context["page"].paginator
+        assert len(paginator_results.object_list) == 0
+        assert test_record not in paginator_results.object_list
+
+    def test_catalog_search_with_inactive_item(self, client: Client):
+        test_record = get_default_record()
+        get_test_item(record=test_record, is_active=False)
+
+        response = client.get(reverse("search"), data={"q": test_record.title})
+        assert response.status_code == 200
+        paginator_results = response.context["page"].paginator
+        assert len(paginator_results.object_list) == 0
+        assert test_record not in paginator_results.object_list
+
+    def test_catalog_ignored_search_terms(self, client: Client):
+        response = client.get(reverse("search"), data={"q": "a an the asdf"})
+        assert response.status_code == 200
+        assert response.context["search_term"] == "asdf"
